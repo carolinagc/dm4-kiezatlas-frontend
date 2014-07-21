01b72ae0a700a5ea21bab39a1be5075ba23cd176
carolina <carolina@sindominio.net>
2014-07-22 01:27:04 +0200

    Set up of plugin

---
+++ src/main/java/de/kiezatlas/frontend/FrontendPlugin.java
@@ -0,0 +1,25 @@
+package de.kiezatlas.frontend;
+
+import de.deepamehta.core.osgi.PluginActivator;
+import de.deepamehta.core.service.Directives;
+import de.deepamehta.core.service.PluginService;
+import de.deepamehta.core.service.annotation.ConsumesService;
+
+import com.sun.jersey.api.view.Viewable;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+@Path("/kiezatlas-frontend/")
+@Produces("text/html")
+    public class FrontendPlugin extends PluginActivator {
+
+@GET
+@Path("index")
+
+	 public Viewable index() {
+	 return new Viewable("/index.html", null);
+	 
+     }
+    }

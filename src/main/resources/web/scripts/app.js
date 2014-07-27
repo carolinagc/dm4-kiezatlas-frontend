e983b3aa812123826c32b2d10efee632c23b3ab7
carolina <carolina@sindominio.net>
2014-07-27 23:41:08 +0200

    Add service to retrieve all criteria from kiezatlas, modify index view to display the criteria in sidebar

--- src/main/resources/web/scripts/app.js
+++ src/main/resources/web/scripts/app.js
@@ -1,18 +1,36 @@
 var app = angular.module('kiezatlasFrontend', []);
 
-app.controller('KiezAtlasController', ['$scope', function($scope){
+app.controller('kiezatlasController', function($scope, frontendService) {
+    frontendService.getAllCriteria(function(criteria) {
+	console.log(criteria);
+	$scope.criteria = criteria;
+    });
 
-}]);
+});
 
-app.directive("leaflet", function(){
+app.directive("leaflet", function() {
     return {
 	restrict: 'E',
 	template: '<div id="map"></div>',
-	link: function() {
+	link:function () {
 	    var map = L.map('map').setView([52.52, 13.41], 16);
 	    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
 		attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 	    }).addTo(map);
+	    var marker = L.marker([52.52, 13.41]).addTo(map).bindPopup('Hello. <br> World?.')
+		.openPopup();
+	    
 	}
     };
+
+
 });
+
+
+app.service("frontendService", function($http) {
+    this.getAllCriteria = function(callback) {
+	$http.get("/site/criteria").success(callback);
+    };
+});
+
+

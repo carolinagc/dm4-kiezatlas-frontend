8a70b03faf2ffbcd6bc777cd5444e86b147b3d23
carolina <carolina@sindominio.net>
2014-08-05 20:36:44 +0200

    Retrieve geoObjects from a category

--- src/main/resources/web/scripts/app.js
+++ src/main/resources/web/scripts/app.js
@@ -1,6 +1,18 @@
+/*
+ViewModel
+    criteria - the criteria shown upper/left (array of topics)
+    currentCriteria - name of selected criteria (string)
+    criteriaCategories - the categories shown in the lower/left (array of topics)
+
+
+*/
+
+
+
 var app = angular.module('kiezatlasFrontend', []);
 
 app.controller('sidebarController', function($scope, frontendService) {
+
     frontendService.getAllCriteria(function(criteria) {
 	$scope.criteria = criteria;
     });
@@ -10,15 +22,20 @@ app.controller('sidebarController', function($scope, frontendService) {
 	frontendService.getCriteriaCategories(selectedCriteria.uri, function(criteriaCategories) {
             $scope.criteriaCategories = criteriaCategories.items;
 	});
-	$scope.showCategoryGeoObjects = function(category){
-	    frontendService.getGeoObjectsByCategory(category.id, function(categoryGeoObjects) {
-		$scope.categoryObjects = [];
-		$scope.categoryGeoObjects = categoryGeoObjects;
-		
-	    });		
-	};
-	
     };
+
+    $scope.showCategoryGeoObjects = function(category){
+	frontendService.getGeoObjectsByCategory(category.id, function(geoObjects) {
+	    console.log(geoObjects);
+	    angular.forEach(geoObjects, function(geoObject) {
+		var geoCoord = geoObject.composite["dm4.contacts.address"].composite["dm4.geomaps.geo_coordinate"].composite;
+		var lon = geoCoord["dm4.geomaps.longitude"].value;
+		var lat = geoCoord["dm4.geomaps.latitude"].value;
+		console.log(lon, lat);
+	    });
+	});		
+    };
+
 });
 
 
@@ -26,7 +43,7 @@ app.directive("leaflet", function() {
     return {
 	restrict: 'E',
 	template: '<div id="map"></div>',
-	link:function () {
+	link: function () {
 	    var map = L.map('map').setView([52.52, 13.41], 16);
 	    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
 		attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'

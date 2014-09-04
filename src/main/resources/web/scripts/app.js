837a5cd5e574298caa28c0c0b1257a70ad5154b4
carolina <carolina@sindominio.net>
2014-09-04 13:47:26 +0200

    Create category list only once. Refactor directive.
    Create category list only the first time. Refactor directive to hide/show category markerlayers. On load set default criteria to Zielgruppe and show its categories list.

--- src/main/resources/web/scripts/app.js
+++ src/main/resources/web/scripts/app.js
@@ -24,11 +24,11 @@ app.controller('sidebarController', function($scope,frontendService, utilService
     
     frontendService.getAllCriteria().then(function(response) {
         $scope.criteria = response.data;
+        $scope.selectCriteria(response.data[4]);
         angular.forEach(response.data, function(criteria) {
             categoryLayers[criteria.uri]=[];
         });
-
-        $scope.selectCriteria(response.data[4]);
+        $scope.categoryLayers = categoryLayers;
     });          
 
 
@@ -44,33 +44,42 @@ app.controller('sidebarController', function($scope,frontendService, utilService
         $scope.currentCategory = category;
         $scope.state="geo object list";
 
-// PENDING Create only if needed selected category propierty and fill it 
+        var loadedCategories = utilService.areCategoriesLoaded($scope.categoryLayers, $scope.currentCriteria.uri, category.uri);
+        if (loadedCategories) {
+            $scope.map.setLayerVisibility(categoryLayers[$scope.currentCriteria.uri][category.uri], true);
+            console.log("areCategoriesLoaded LALALLALALA");
+        } else {
 
-        categoryLayers[$scope.currentCriteria.uri][category.uri] = [];            
-        categoryLayers[$scope.currentCriteria.uri][category.uri].push(category);            
+// Create only if needed selected category propierty and fill it 
 
-        console.log("CATEGORY LAYERS in selectCategory", categoryLayers);
-        frontendService.getGeoObjectsByCategory(category.id).then(function(response) {
-            $scope.geoObjects = response.data;
-            console.log("GEOBJECTS IN GEO OBJECT LIST", response.data);
-
+            categoryLayers[$scope.currentCriteria.uri][category.uri] = [];            
+            categoryLayers[$scope.currentCriteria.uri][category.uri]["layer"] = {};
+            categoryLayers[$scope.currentCriteria.uri][category.uri].push(category);            
+            
+            console.log("CATEGORY LAYERS in selectCategory", categoryLayers);
+            frontendService.getGeoObjectsByCategory(category.id).then(function(response) {
+                $scope.geoObjects = response.data;
+                console.log("GEOBJECTS IN GEO OBJECT LIST", response.data);
+                $scope.markersLayer = $scope.map.createLayer();
 //Creating markers layer for the category selected
 
-            angular.forEach(response.data, function(geoObj) {
-                var geoCoord = geoObj.composite["dm4.contacts.address"].composite["dm4.geomaps.geo_coordinate"].composite;
-                var lon = geoCoord["dm4.geomaps.longitude"].value;
-                var lat = geoCoord["dm4.geomaps.latitude"].value;
-            //                          console.log(lat, lon);
-                var coord=[lat,lon];
-                console.log("coord",  coord);
-                var categoryLayer = categoryLayers[$scope.currentCriteria.uri][category.uri];
-                $scope.map.addMarker(categoryLayer, coord, geoObj.id);
-                $scope.map.setLayerVisibility(categoryLayer, true);
-            });
-            $scope.categoryLayers = categoryLayers;
-            console.log("categoryLayers", categoryLayers);
+                angular.forEach(response.data, function(geoObj) {
+                    var geoCoord = geoObj.composite["dm4.contacts.address"].composite["dm4.geomaps.geo_coordinate"].composite;
+                    var lon = geoCoord["dm4.geomaps.longitude"].value;
+                    var lat = geoCoord["dm4.geomaps.latitude"].value;
+                    var coord=[lat,lon];
+                    console.log("coord",  coord);
+//                    var categoryLayer = categoryLayers[$scope.currentCriteria.uri][category.uri]["layer"];
+                    $scope.map.addMarker(categoryLayers, category.uri, coord, geoObj.id);
 
-        });     
+                });
+                
+                categoryLayers[$scope.currentCriteria.uri][category.uri]["layer"] = $scope.markersLayer;
+                $scope.map.setLayerVisibility(categoryLayers[$scope.currentCriteria.uri][category.uri], true);
+                $scope.categoryLayers = categoryLayers;
+                console.log("categoryLayers", categoryLayers);
+            });     
+        };
     };
 
     $scope.showGeoObjectDetails = function(geoObjectId) {
@@ -133,7 +142,7 @@ app.directive("leaflet", function() {
         template: '<div id="map"></div>',
         link: function(scope) {
             console.log("link function called")
-            var layers = L.layerGroup();
+
             var baseLayer = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
                 attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
             });
@@ -147,36 +156,29 @@ app.directive("leaflet", function() {
 
             scope.map = {
                 createLayer: function() {
-                    return  L.layerGroup();
-                    
+                    return L.layerGroup()
                 },
-
-                addMarker: function(categoryLayer, coord, geoObjectId) {
-                    var markersLayer = L.layerGroup();
-                    var marker = L.marker(coord).addTo(markersLayer).on('click', onclick(geoObjectId));
+                addMarker: function(categoryLayers, categoryUri, coord, geoObjectId) {
+                    var marker = L.marker(coord).addTo(scope.markersLayer).on('click', onclick(geoObjectId));
                     function onclick(geoObjectId) {
                         return function() {
                             scope.showGeoObjectDetails(geoObjectId);
                         };
                     };
 
-                    categoryLayer.push(markersLayer);            
-                    markersLayer.addTo(map);
-
                 },
                 setLayerVisibility: function(categoryLayer, visibility ) {
                     if (visibility) {
-                        if (!map.hasLayer(categoryLayer)) {
-                            map.addLayer(categoryLayer[1]);
+                        if (!map.hasLayer(categoryLayer["layer"])) {
+                            map.addLayer(categoryLayer["layer"]);
                         }
                     
                     } else {
-                        if (map.hasLayer(categoryLayer)) {
-                            map.removeLayer(categoryLayer[1]);
+                        if (map.hasLayer(categoryLayer["layer"])) {
+                            map.removeLayer(categoryLayer["layer"]);
                         }
                     }
                 }
-                
             }
         }
     };
@@ -223,7 +225,7 @@ app.service("utilService", function() {
         return Object.prototype.toString.call(obj) == "[object Array]"
     };
 
-    this.areCategoriesLoaded = function (criteriaUri){
-
+    this.areCategoriesLoaded = function (categoryLayers, criteriaUri, categoryUri){
+        return categoryLayers[criteriaUri][categoryUri]
     }
 });

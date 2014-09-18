66a6448c58dbde4bf8d1e2fc11cf883326a69774
carolina <carolina@sindominio.net>
2014-09-18 21:49:03 +0200

    Config karma and  first dummy test

---
+++ src/main/resources/web/tests/appSpec.js
@@ -0,0 +1,8 @@
+'use strict';
+describe("Testing the controller sidebarController", function(){
+
+    it("contains spec with an expectation", function() {
+        expect(true).toBe(true);
+    });
+
+});
